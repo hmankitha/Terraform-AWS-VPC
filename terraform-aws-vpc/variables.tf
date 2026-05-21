88582f93ad24333a48703673d4801f036905be60
Adii <adii...@Adiis-MacBook-Air.local>
2026-05-21 15:23:53 +0530

    terraform

--- terraform-aws-vpc/variables.tf
+++ terraform-aws-vpc/variables.tf
@@ -2,8 +2,12 @@ variable "project" {
     type = string
 }
 
-variable "environment"{
+variable "environment" {
     type = string
+    validation {
+        condition     = contains(["dev", "qa", "uat", "prod"], var.environment)
+        error_message = "Environments should be one of dev, qa, uat or prod"
+    }
 }
 
 variable "vpc_cidr" {
@@ -19,4 +23,64 @@ variable "vpc_tags" {
 variable "igw_tags" {
     type = map
     default = {}
+}
+
+variable "public_subnet_cidrs" {
+    type = list
+    default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "public_subnet_tags" {
+    default = {}
+    type = map
+}
+
+variable "private_subnet_cidrs" {
+    type = list
+    default = ["10.0.11.0/24", "10.0.12.0/24"]
+}
+
+variable "private_subnet_tags" {
+    default = {}
+    type = map
+}
+
+variable "database_subnet_cidrs" {
+    type = list
+    default = ["10.0.21.0/24", "10.0.22.0/24"]
+}
+
+variable "database_subnet_tags" {
+    default = {}
+    type = map
+}
+
+variable "public_route_table_tags" {
+    default = {}
+    type = map
+}
+
+variable "private_route_table_tags" {
+    default = {}
+    type = map
+}
+
+variable "database_route_table_tags" {
+    default = {}
+    type = map
+}
+
+variable "eip_tags" {
+    default = {}
+    type = map
+}
+
+variable "nat_gateway_tags" {
+    default = {}
+    type = map
+}
+
+variable "is_peering_required" {
+    default = false
+    type = bool
 }
