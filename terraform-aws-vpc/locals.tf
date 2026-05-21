0aa9e90db7d7311a80123608f3f675387b55bb6a
Adii <adii...@Adiis-MacBook-Air.local>
2026-05-21 13:26:23 +0530

    terraform

--- terraform-aws-vpc/locals.tf
+++ terraform-aws-vpc/locals.tf
@@ -1,15 +1,15 @@
 locals {
-    common_tags = {
-       Project = var.Project
-       Environment = var.environment
-       Terrafrom = "true"
+  common_tags = {
+    Project     = var.project
+    Environment = var.environment
+    Terraform   = "true"
+  }
 
-    }
-    vpc_final_tags = merge(
-                            local.common_tags,
-                            {
-                                Name = "${var.project}-${var.environment}"
-                            },
-                            var.vpc_tags
-                        )
+  vpc_final_tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}"
+    },
+    var.vpc_tags
+  )
 }
--- terraform-aws-vpc/main.tf
+++ terraform-aws-vpc/main.tf
@@ -1,4 +1,4 @@
-resource "main" {
+resource "aws_vpc" "main" {
     cidr_block = var.vpc_cidr
     instance_tenancy = "default"
     enable_dns_hostnames = true
